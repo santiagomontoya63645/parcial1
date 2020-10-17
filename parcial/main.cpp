ba218c05963e922fe8422a8783e500423ec866d7
santiagomontoya63645 <62150521+santiagomontoya63645@users.noreply.github.com>
2020-10-17 16:39:31 -0500

    actualizacion con analisis
    tuve el problema de no ingresar el avanze del analisis en el documento de git, asi quew los inclui dentro del codigo

--- parcial/main.cpp
+++ parcial/main.cpp
@@ -1,40 +1,56 @@
 #include <iostream>
 #include <math.h>
-//d debe tener posiciocon fija
+//ingreso datos iniciales para posiciones angulos y  velocidades ppara cada cañon junto con la altura fija del cañon defensivo,
+//teniendo en cuenta los limites para que no ocurra el caso dew que el cañon ofensivo lance la bala y al milisegundo explote  porque tenia el cañon al lado
+//despues verificar que el cañon defensivo no este en el radio de explosion
+//deboo aseguirarme de quee ambos cañones no sewan destruidos, debo aumentar el tiempo para que las balas no queden flotando
+//PARA QQUE ESTYO NO OCURRA PONDRE EL LIMITE DE LA POSICION DEL CAÑO N DEFENSIVO MAXIMO EN 10 EN X Y LA MINIMA DEL CAÑON EN 30 osea una distancia minima de 20
+// para la defensa considerando el tiempo de respuestapara asegurar la defensa tendra una velocidad mayor a la ofensiva
+// debo estar siempre actualizando posiciones para nuevos disparos
 using namespace std;
 bool verificadordeposicionparadisparar(int a, int b);
-int posicionx(int x, int angulo, int tiempo, int velocidad);
-float posiciony(int y, int angulo, int tiempo,int velocidad);
+float posicionx(float x, float angulo, float tiempo, float velocidad);
+float posiciony(float y, float angulo, float tiempo,float velocidad);
 int main()
 {
-    int xdo=0,  angulodo=0,vdo=0,xD,tiempo=0,velocidadxO=3, velocidadxD=10;
+    float xdo=0,auxxD,auxyD,  angulodo=0,vdo=0,xD,tiempo=0,velocidadxO=3, velocidadxD=10,auxdo,auxydo;
     bool m;
     float ydo=0,yD;
     while (true){
-        cout<<"ingrese posicion de disparo Xo"<<endl;cin>>xdo;
+        cout<<"ingrese posicion de disparo Xo menor a 10"<<endl;cin>>xdo;
         cout<<"ingrese posicion de disparo Yo"<<endl;cin>>ydo;
+        auxdo=xdo;
+        auxydo=ydo;
         cout<<"ingrese angulo de el cañon agresivo"<<endl;cin>>angulodo;
         cout<<"ingrese velocidad inicial del cañon agresivo"<<endl;cin>>vdo;
-        cout<<"ingrese posicion de disparo xD"<<endl;cin>>xD;
+        cout<<"ingrese posicion de disparo xD mayor a 20"<<endl;cin>>xD;
         cout<<"ingrese posicion de disparo yD"<<endl;cin>>yD;
+        auxxD=xD;
+        auxyD=yD;
+
         m=verificadordeposicionparadisparar(xdo,xD);
+        xdo=posicionx(xdo,angulodo,tiempo,velocidadxO);
+        ydo=posiciony(ydo,angulodo,tiempo,velocidadxO);
+        tiempo++;
         if (m==true){
+            //boom
+
 
         }
 
     }
     return 0;
 }
-bool verificadordeposicionparadisparar(int a, int b){
-    int distancia=20;
-    if(b==0.05*distancia){
+bool verificadordeposicionparadisparar(float a, float b){
+
+    if(a==0.05*b){
         return true;
     }
     else{
         return false;
     }
 }
-int posicionx(int x, int angulo,int tiempo, int velocidad){
+float posicionx(int x, int angulo,int tiempo, int velocidad){
     int posx=0;
     posx=x+velocidad*cos(angulo)*tiempo;
     return posx;
