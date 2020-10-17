e7ff72a2095209b44166679e79ce15baba6b2116
santiagomontoya63645 <62150521+santiagomontoya63645@users.noreply.github.com>
2020-10-17 15:22:37 -0500

    ingreso de funcion verificadora
    funcion verificadora si el cañon ofensivo detecto al cañon defensivo

--- parcial/main.cpp
+++ parcial/main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 //d debe tener posiciocon fija
 using namespace std;
+bool verificadordeposicionparadisparar(int a, int b);
 int disparodo();
 int main()
 {
-    int xdo=0, ydo=0;
+    int xdo=0, ydo=0, angulodo=0,vdo=0,xD,yD;
+    bool m;
     while (true){
         cout<<"ingrese posicion de disparo Xo"<<endl;cin>>xdo;
         cout<<"ingrese posicion de disparo Yo"<<endl;cin>>ydo;
+        cout<<"ingrese angulo de el cañon agresivo"<<endl;cin>>angulodo;
+        cout<<"ingrese velocidad inicial del cañon agresivo"<<endl;cin>>vdo;
+        cout<<"ingrese posicion de disparo xD"<<endl;cin>>xD;
+        cout<<"ingrese posicion de disparo yD"<<endl;cin>>yD;
+        m=verificadordeposicionparadisparar(xdo,xD);
+        if (m==true){
+
+        }
+
     }
     return 0;
+}
+bool verificadordeposicionparadisparar(int a, int b){
+    if(a==0.05*b){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
