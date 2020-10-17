9f28a450dd1447662eabe0cdf032fb390c35418b
santiagomontoya63645 <62150521+santiagomontoya63645@users.noreply.github.com>
2020-10-17 17:25:13 -0500

    prueba de explosion cañon defensivo
    estaba probando si algun momento con el codigo y diferentes datos en cuallquier momento una bala puede destruir el cañon y si hizo boooom

--- parcial/main.cpp
+++ parcial/main.cpp
@@ -8,54 +8,69 @@
 // para la defensa considerando el tiempo de respuestapara asegurar la defensa tendra una velocidad mayor a la ofensiva
 // debo estar siempre actualizando posiciones para nuevos disparos
 using namespace std;
-bool verificadordeposicionparadisparar(int a, int b);
-float posicionx(float x, float angulo, float tiempo, float velocidad);
-float posiciony(float y, float angulo, float tiempo,float velocidad);
+bool boom(float a, float b);
+float posicionx(float x, int angulo, float tiempo, float velocidad);
+float posiciony(float y, int angulo, float tiempo,float velocidad);
 int main()
 {
-    float xdo=0,auxxD,auxyD,  angulodo=0,vdo=0,xD,tiempo=0,velocidadxO=3, velocidadxD=10,auxdo,auxydo;
+    float xdo=0,auxxD,auxyD,  vdo=0,xD,tiempo=0,velocidadxO=3, velocidadxD=10,auxdo,auxydo;
     bool m;
+    float t=2.5;
     float ydo=0,yD;
+    int f=0,anguloD,angulodo=0;
     while (true){
         cout<<"ingrese posicion de disparo Xo menor a 10"<<endl;cin>>xdo;
         cout<<"ingrese posicion de disparo Yo"<<endl;cin>>ydo;
         auxdo=xdo;
         auxydo=ydo;
         cout<<"ingrese angulo de el cañon agresivo"<<endl;cin>>angulodo;
-        cout<<"ingrese velocidad inicial del cañon agresivo"<<endl;cin>>vdo;
+       // cout<<"ingrese velocidad inicial del cañon agresivo"<<endl;cin>>vdo;
         cout<<"ingrese posicion de disparo xD mayor a 20"<<endl;cin>>xD;
         cout<<"ingrese posicion de disparo yD"<<endl;cin>>yD;
+        cout<<"ingrese angulo de el cañon agresivo"<<endl;cin>>anguloD;
         auxxD=xD;
         auxyD=yD;
 
-        m=verificadordeposicionparadisparar(xdo,xD);
-        xdo=posicionx(xdo,angulodo,tiempo,velocidadxO);
-        ydo=posiciony(ydo,angulodo,tiempo,velocidadxO);
-        tiempo++;
+
+        while(f==0){
+            xdo=posicionx(xdo,angulodo,tiempo,velocidadxO);
+            ydo=posiciony(ydo,angulodo,tiempo,velocidadxO);
+            tiempo++;
+            m=boom(xdo,xD);
+           // if (tiempo>=2){
+             //   m=boom(xdo,xD);
+               // xD=posicionx(xD,anguloD,t,velocidadxD);
+                //yD=posiciony(yD,anguloD,t,velocidadxD);
+                //t=t+1;
+            //}
         if (m==true){
-            //boom
+            cout<<"boom"<<endl;
+            f=1;
 
 
         }
-
+        else{
+            cout<<"dios esta aqui esta aqui tanccerca como el aire que respirooooo"<<endl;
+        }
+     }
     }
     return 0;
 }
-bool verificadordeposicionparadisparar(float a, float b){
+bool boom(float a, float b){
 
-    if(a==0.05*b){
+    if(a<=0.05*b){
         return true;
     }
     else{
         return false;
     }
 }
-float posicionx(int x, int angulo,int tiempo, int velocidad){
-    int posx=0;
+float posicionx(float x, int angulo,float tiempo, float velocidad){
+    float posx=0;
     posx=x+velocidad*cos(angulo)*tiempo;
     return posx;
 }
-float posiciony(int y, int angulo, int tiempo, int velocidad){
+float posiciony(float y, int angulo, float tiempo, float velocidad){
     float posy=0;
     posy=y+(velocidad*sin(angulo)-9.8*tiempo)-9.8/2*tiempo*tiempo;
     return posy;
